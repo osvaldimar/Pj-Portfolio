2aedd855a9dcd92f6795b9edbc42803b22342dfe
osvaldimar <osvaldimar1@gmail.com>
2017-01-29 21:35:11 -0200

    classe usuario

---
+++ portfolio/portfolio-domain/src/main/java/br/com/osvald/portfolio/domain/Usuario.java
@@ -0,0 +1,25 @@
+package br.com.osvald.portfolio.domain;
+
+public class Usuario {
+
+	
+	private String nome;
+	private Integer idade;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+}
